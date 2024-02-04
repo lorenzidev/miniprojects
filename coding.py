218c801d7c828e7c5186882b7e5a8cff8160ea28
lorenzidev <lorenzidev@gmail.com>
2024-02-03 19:13:04 -0500

    none

---
+++ coding.py
@@ -0,0 +1,7 @@
+# youtuber = "Mr Beast"
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+# control + / para comentar várias linhas de código de uma vez.
---
+++ madlibs.py
@@ -0,0 +1,16 @@
+
+# youtuber = "Mr Beast"
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+adj = input("What's the adjective? \n")
+verb1 = input("What's the verb? \n")
+verb2 = input("Give me another verb. \n")
+famous_person = input("What's the most famous person for you? \n")
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
