6745b53b813ba8f52c07f1e11ffa201ea081dda3
lorenzidev <lorenzidev@gmail.com>
2024-02-03 19:48:45 -0500

    none

--- guesscomputer.py
+++ guesscomputer.py
@@ -1,3 +1,5 @@
+# Temos dois modelos de jogos nesse arquivo, um com outro user, e o segundo com o computador.
+
 import random
 
 def guess(x):
@@ -13,4 +15,24 @@ def guess(x):
 
     print(f'You got it! Winner!' The number was {random_number})
 
+
+
 guess(100)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} to high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Yay! The computer guessed your number, {guess} correctly!')
+
+computer_guess(100)
