60df0d119e94c3820ea9f5838f5fa59982e919ca
lorenzidev <lorenzidev@gmail.com>
2024-02-06 00:46:20 -0500

    none

--- analysis.py
+++ analysis.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
-# Substitua 'caminho/para/seu/arquivo.xlsx' pelo caminho real do seu arquivo Excel
-arquivo_excel = 'C:/Users/Victor/Desktop/python/data for january.xlsx'
-
+# Substitua pelo caminho real do seu arquivo Excel
+arquivo_excel = 'C:/Users/Victor/Desktop/python/data_for_january.xlsx'
 df = pd.read_excel(arquivo_excel)
 
-# Descomente se quiser ver o DataFrame completo no console
-# pd.set_option('display.max_columns', None)
-
-# Calcula a porcentagem de cada valor único na coluna 'General.Authenticated' e multiplica por 100 para obter a porcentagem
-general_authenticated = df['General.Authenticated'].value_counts(normalize=True) * 100
-#general_process = df['General.Process'].value_counts(normalize=True) * 100
-
-# Itera sobre a série de porcentagens para imprimir cada valor único e sua porcentagem correspondente
-for resultado, porcentagem in general_authenticated.items():
-    print(f"{resultado}: {porcentagem:.2f}%")
+# Agrupa pelo nome da campanha primeiro e depois itera sobre cada grupo
+for nome_campanha, grupo_campanha in df.groupby('CAMPAIGN'):
+    print(f"CAMPAIGN {nome_campanha}")
+    
+    # Calcula a porcentagem para 'Confirmed' e 'Failed' dentro do grupo atual
+    porcentagens = grupo_campanha['General.Authenticated'].value_counts(normalize=True) * 100
+    
+    # Imprime as porcentagens formatadas
+    for resultado, porcentagem in porcentagens.items():
+        print(f"{resultado}: {porcentagem:.2f}%")
+    
+    # Espaço extra para separar as campanhas na saída
+    print()
---
+++ while.py
@@ -0,0 +1,8 @@
+var = 1
+
+while var <= 100:
+    if var % 2 == 0:
+        print(f'Par: {var}')
+    else:
+        print(f'Impar: {var}')
+    var += 1
